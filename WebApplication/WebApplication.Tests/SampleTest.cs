ba98fb3b271ae8ea9a76911b336f894cabe9b147
Piran Tata <piran.tata@vanquisbank.co.uk>
2020-01-21 15:04:17 +0000

    added test sln

---
+++ WebApplication/WebApplication.Tests/SampleTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace WebApplication.Tests
+{
+    public class SampleTest
+    {
+        [Fact]
+        public void DoTest()
+        {
+            var a = 1;
+            var b = 1;
+            
+            Assert.Equal(a,b);
+        }
+    }
+}
